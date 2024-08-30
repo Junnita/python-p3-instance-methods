2c6fb8d48f5d8d92a7c7623f46f85db969cf9726
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-08-30 12:57:44 +0300

    done

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
-class Person:
-    # Class body goes here
 
-    #Instance method definition
+class Person:
     pass
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+
+mark = Person()
+mark.talk()
+john = Person()
+john.walk()
